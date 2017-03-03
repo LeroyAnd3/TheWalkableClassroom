76d017f533700bc5b0cd951a63ea26717fbfb9bd
schatzmana <schatzmana@gmail.com>
2017-03-02 22:54:25 -0500

    Adding Abby’s work on the Concentration game UI

---
+++ concentration.js
@@ -0,0 +1,100 @@
+var paused;
+
+function checkBoardConfig(){
+  $('#default').each(function() {
+    if (this.selected) {
+      // alert('the default option has been selected');
+      makeRectConfig();
+      //$('#cards').append();
+    }
+  });
+
+  $('#rectangle').each(function() {
+    if (this.selected) {
+      // alert('the rectangle option has been selected');
+      makeRectConfig();
+      //$('#cards').append();
+    }
+  });
+
+  $('#square').each(function() {
+    if (this.selected) {
+      // alert('the square option has been selected');
+      makeSqConfig();
+    }
+  });
+}
+
+function makeRectConfig(){
+  // alert("making rectangular config");
+  var row, i, j, x;
+  width = 6;
+
+  for (i=0; i<3; i++){
+    x = ( width * i);
+    row = '<div id="row">';
+
+    for (j=1; j<(width+1); j++){
+      row += '<div class ="card" id="card' + (x+j) +'">Card ' + (x+j) +'</div>';
+    }
+    row += '</div>';
+    $('#deck').append(row);
+  }
+}
+
+function makeSqConfig(){
+  // alert("making square config");
+  var row, i, j, x;
+  width = 3;
+
+  for (i=0; i<(width); i++){
+    x = ( width * i);
+    row = '<div id="row">';
+
+    for (j=1; j<(width +1); j++){
+      row += '<div class ="card" id="card' + (x+j) +'">Card ' + (x+j) +'</div>';
+    }
+    row += '</div>';
+    $('#deck').append(row);
+  }
+}
+
+$(document).ready( function(){
+  $('#play').click( function() {
+    // alert("play clicked");
+     paused = 0; // 0 = no, 1 = yes
+
+    $("#play").attr("disabled", "disabled");
+    $("#gameScore").val(0);
+
+    checkBoardConfig();
+  });
+
+
+  $('#quit').click( function() {
+    $("#play").attr("disabled", "disabled");
+    $("#pause").attr("disabled", "disabled");
+    $("#quit").attr("disabled", "disabled");
+  });
+
+
+  $('#restart').click( function() {
+    window.location.reload();
+  });
+
+  $('#pause').click( function() {
+    if (paused == 0) { // in game play
+      // stop timer
+      alert("pausing game");
+      paused = 1;
+    }
+    else if (paused == 1){ // game play is already paused
+      // restart timer
+      alert("resuming game");
+      paused = 0;
+    }
+    else {
+      alert("paused is not 0 or 1");
+    }
+  });
+});
