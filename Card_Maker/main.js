1a5a1cc6e04f54773e8373a4e1ca459cfc992462
Elliot Chen <Elliot Chen>
2017-03-26 12:28:59 -0400

    fixed bug in making new category

--- Card_Maker/main.js
+++ Card_Maker/main.js
@@ -1,8 +1,7 @@
 
 'use strict'
 $(document).ready(function(){
-  //Shortcuts to DOM Elements
-//  // alert("in main.js ");
+  //Shortcuts to DOM Element
   var category = document.getElementById('category');
   var answer = document.getElementById('answer');
   var hint1 = document.getElementById('hint1');
@@ -21,11 +20,9 @@ $(document).ready(function(){
 
   submitButton.addEventListener("click",function(e){
     e.preventDefault();
-  //  // alert("submit button worked");
     hintList = pushHints(hint1.value,hint2.value,hint3.value,
               hint4.value,hint5.value,hint6.value,hint7.value,hint8.value);
     createNewTerm(rootRef);
-
   });
 
   //create new term that will be submitted
@@ -35,18 +32,15 @@ $(document).ready(function(){
 
     categoryRef.once('value').then(function(categoryKeys){//check if subject exist
       categoryKeys.forEach(function(snapSubjects){
-        // alert("in forEach statement");
-        //console.log(snapSubjects.val().subject);
         var k = snapSubjects.val();
         if(k.subject===category.value){
-          // alert("found a matching term");
           key = snapSubjects.key
-          // alert("key: " + key);
           return pushTerm(key,answer.value,rootRef,hintList);
         }
       });
     }).then(function(data){
         if(key===null){//if no match was found then pushTerm and create new category
+          key = createNewCategory(rootRef);
           return pushTerm(key,answer.value,rootRef,hintList);
         }
       },function(error){
@@ -56,18 +50,12 @@ $(document).ready(function(){
   }
 
   function pushTerm(key,answer,rootRef,hintList){
-    if(key===null){//create new category
-      key = createNewCategory(rootRef);
-    }
-    // alert("entered writeNewTerm");
     //A Term entry
     var termData = {
       term:answer,
       hint:hintList
     };
-    var newTermKey = firebase.database().ref().child("terms").push().key;
-    // alert("term key: " + newTermKey);
-    // alert("category key: " + key);
+
     //Write the new term's data to the database
     var updates = {};
     updates['/terms/' + answer] = termData;
@@ -77,7 +65,6 @@ $(document).ready(function(){
   }
 
   function createNewCategory(rootRef){
-    // alert("entered new category");
     //A category entry
     var categoryData = {
       subject: category.value
@@ -87,15 +74,13 @@ $(document).ready(function(){
 
     //Write new category data
     var updates = {};
-    updates['/category/' + newCategoryKey] = categoryData;
+    updates['/categories/' + newCategoryKey] = categoryData;
     firebase.database().ref().update(updates);
-    // alert("end of new category");
     return newCategoryKey;
   }
 
   //push hints that our not null to an array
   function pushHints(h1, h2, h3, h4, h5, h6, h7, h8){
-      //  // alert("entered push hint function");
     var list = {
         hint1:h1,
         hint2:h2,
@@ -106,11 +91,8 @@ $(document).ready(function(){
         hint7:h7,
         hint8:h8
       };
-      console.log(list);
-      // alert("pause after push hints");
     return list;
-    //  // alert("end of push hint function. Size: " + array.length);
-
   }
+
 
 });
---
+++ Concentration_Game/firebase.js
@@ -0,0 +1,14 @@
+//retrieve a set a terms for a category
+let handles = [];
+function getCategoryTerms(category,cb){
+  let rootRef = firebase.database().ref();
+  let cateRef = rootRef.child('category').;
+
+  cateRef.child()
+
+
+    }
+  });
+
+
+}
